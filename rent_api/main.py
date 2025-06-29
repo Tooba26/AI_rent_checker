a90af1639dbae8c6a767f6e258eb58acef6c4a70
ToobaMehboob <st125404@ait.asia>
2025-06-29 15:59:48 +0700

    API code

---
+++ rent_api/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import pandas as pd
+
+app = FastAPI()
+model = joblib.load('model/house_rent_model.pkl')
+
+# Define the input data model
+class RentRequest(BaseModel):
+    Size: int
+    BHK: int
+    Bathroom: int
+    Current_Floor: int
+    Total_Floors: int
+    Area_Type: str
+    Furnishing_Status: str
+    Tenant_Preferred: str
+    City: str
+
+
+@app.get("/")
+def read_root():
+    return {"message": "House Rent Prediction API is live!"}
+
+@app.post("/predict_rent")
+def predict_rent(data: RentRequest):
+    df = pd.DataFrame([data.dict()])
+    rent = model.predict(df)[0]
+    return {"Predicted_Rent": round(rent, 2)}
